04fa6b50f58f0c2cf46b38859798b57b9d6ef1bc
ssha27 <stsha2023@gmail.com>
2025-02-25 11:24:01 -0800

    initial commit

---
+++ PE.v
@@ -0,0 +1,15 @@
+
+
+module PE(
+    inp_north, 
+    inp_west, 
+    clk, 
+    rst, 
+    outp_south, 
+    outp_east, 
+    result);
+
+
+// Your design is here
+
+endmodule
---
+++ systolic_array.v
@@ -0,0 +1,35 @@
+module systolic_array(
+    inp_west0,
+    inp_west4,
+    inp_west8,
+    inp_west12,
+    inp_north0,
+    inp_north1,
+    inp_north2,
+    inp_north3,
+    out00,
+    out01,
+    out02,
+    out03,
+    out04,
+    out05,
+    out06,
+    out07,
+    out08,
+    out09,
+    out10,
+    out11,
+    out12,
+    out13,
+    out14,
+    out15,
+	clk, 
+    rst, 
+    done);
+
+// Your design is here
+
+
+
+
+endmodule
