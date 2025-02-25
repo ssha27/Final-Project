d7fdc7a5ced243cc1acdf6d0e46f1396c7c3c1ff
ssha27 <stsha2023@gmail.com>
2025-02-25 15:59:07 -0800

    fixed assignment errors in PE.v

--- PE.v
+++ PE.v
@@ -1,21 +1,24 @@
 module PE(
-  input [7:0] inp_north, inp_west, 
-  input clk, rst, 
-  output [7:0] outp_south, outp_east, 
-  output [15:0] result);
-  
+  input [7:0] inp_north, inp_west,
+  input clk, rst,
+  output reg [7:0] outp_south, outp_east,
+  output reg [15:0] result
+);
+
   reg [15:0] internal;
-  
-  assign internal = rst ? 0 : internal;
-  
-  always @ (clk) begin
-    
-  	internal += inp_north * inp_west;
-    
-    outp_south = inp_north;
-    outp_east = inp_west;
-    result = internal;
-    
+
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      internal   <= 16'd0;
+      outp_south <= 8'd0;
+      outp_east  <= 8'd0;
+      result     <= 16'd0;
+    end else begin
+      internal   <= internal + (inp_north * inp_west);
+      outp_south <= inp_north;
+      outp_east  <= inp_west;
+      result     <= internal;
+    end
   end
-  
+
 endmodule
