0d9ad16bfde5369740bf8b5f84eefeddd2bb9645
ssha27 <stsha2023@gmail.com>
2025-02-25 11:28:26 -0800

    copied changes from eda playground

--- PE.v
+++ PE.v
@@ -1,15 +1,21 @@
-
-
 module PE(
-    inp_north, 
-    inp_west, 
-    clk, 
-    rst, 
-    outp_south, 
-    outp_east, 
-    result);
-
-
-// Your design is here
-
+  input [7:0] inp_north, inp_west, 
+  input clk, rst, 
+  output [7:0] outp_south, outp_east, 
+  output [15:0] result);
+  
+  reg [15:0] internal;
+  
+  assign internal = rst ? 0 : internal;
+  
+  always @ (clk) begin
+    
+  	internal += inp_north * inp_west;
+    
+    outp_south = inp_north;
+    outp_east = inp_west;
+    result = internal;
+    
+  end
+  
 endmodule
--- systolic_array.v
+++ systolic_array.v
@@ -1,14 +1,8 @@
 module systolic_array(
-    inp_west0,
-    inp_west4,
-    inp_west8,
-    inp_west12,
-    inp_north0,
-    inp_north1,
-    inp_north2,
-    inp_north3,
-    out00,
-    out01,
+  input [31:0]inp_west0, inp_west4, inp_west8, inp_west12,
+  			 inp_north0, inp_north1, inp_north2, inp_north3,
+  input clk, rst, 
+  output [15:0] out00, out01,
     out02,
     out03,
     out04,
@@ -23,12 +17,13 @@ module systolic_array(
     out13,
     out14,
     out15,
-	clk, 
-    rst, 
-    done);
+  output done);
 
 // Your design is here
-
+  always @ (clk) begin
+    PE pe1(inp_north[7:0], inp_west[7:0], clk, rst, outsouth1, outeast1, out00)
+    PE pe2(inp_north[15:8], outeast1, clk, rst, outsouth2, outeast2, out00)
+  end
 
 
 
